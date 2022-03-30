61f63acfa791325b83d83dba5c5abb6f010d6cdf
Shwetapoojari <shwetaap07@gmail.com>
2022-03-30 23:14:18 +0530

    First commit by X

---
+++ Demo/src/test/test1.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		int a=23;
+		int b=7;
+		int c = a + b;
+		System.out.println("Addition is :" +c);
+	}
+
+}
---
+++ Demo/src/test/test2.java
@@ -0,0 +1,14 @@
+package test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		String test = "Welcome to Java";
+		if(test.contains("Java")) 
+		{
+			System.out.println("Found!!!!!!!!!");
+		}
+			
+	}
+
+}
